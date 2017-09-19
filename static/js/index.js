dff864a8502b270536ca0d0f1ad7af1d7b93c91a
qingfawang <qingfawang@gmail.com>
2017-09-19 15:52:10 +0800

    stles ,lists,searchs,tags,cooooool

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,29 @@
-$(document).ready(function(){
+jQuery.expr[':'].icontains = function (a, i, m) {
+    return jQuery(a).text().toUpperCase()
+            .indexOf(m[3].toUpperCase()) >= 0;
+};
+
+$(document).ready(function () {
 //     $('#carousel-example-generic').carousel({
 //   interval: 2000
 // })
-})
+    $('#searchPlatform').on('input', function (e) {
+        var text = $(this).val().trim();
+        refreshDataList(text)
+
+    });
+    $('.searchTags span').click(function (e) {
+         var text = $(this).text().trim();
+         $('#searchPlatform').val(text);
+         refreshDataList(text);
+    });
+
+    function refreshDataList(text) {
+        if ("" === text) {
+            $(".bs-callout").removeClass('hidden');
+        }
+
+        $(".bs-callout:not(:icontains(" + text + "))").addClass('hidden');
+        $(".bs-callout:icontains(" + text + ")").removeClass('hidden');
+    }
+});
