9a9dae6e3c784d44c2c017b8bbb4e4ba8b0d71c2
qingfawang <qingfawang@gmail.com>
2017-10-31 17:33:50 +0800

    gzh pic & header

--- miniBond/views.py
+++ miniBond/views.py
@@ -9,80 +9,90 @@ import os
 import uuid
 from datetime import datetime
 
-def hello(request):
-	message = 'hello'
-	return HttpResponse("Hello world")
 
-def myname(request,offset):
-	name = "wqfaa"
-	return render(request,'miniBond/sample.html', locals())
+def hello(request):
+    message = 'hello'
+    return HttpResponse("Hello world")
+
+
+def myname(request, offset):
+    name = "wqfaa"
+    return render(request, 'miniBond/sample.html', locals())
+
 
 def index(request):
-	allpfs=Platform.objects.filter(isValid=True)
-	pfs=[p for p in allpfs if p.promotioninfo_set.filter(isValid=True).count()>0 or (hasattr(p,'linktowx') and p.linktowx.isValid)]
-	contextDict={'pfs':pfs}
+    allpfs = Platform.objects.filter(isValid=True)
+    pfs = [p for p in allpfs if p.promotioninfo_set.filter(
+        isValid=True).count() > 0 or (hasattr(p, 'linktowx') and p.linktowx.isValid)]
+    contextDict = {'pfs': pfs}
 
-	templateName="index.html"
-	staticView(contextDict, templateName,templateName)
-	response=render(request, "static/"+templateName)
-	if "cookieId" not in request.COOKIES:
-		response.set_cookie("cookieId", uuid.uuid4())
-	return response
-
-def staticView(contextDict, templateName,staticFileName):
-	static_html = 'templates/static/' + staticFileName
-	if not os.path.exists(static_html) or True:
-		content = render_to_string('miniBond/' + templateName, contextDict)
-		with open(static_html, 'w', encoding="utf-8") as static_file:
-			static_file.write(content)
-
-def logTrace(request,areaType,target,propertyData):
-	trace = ClickTrace()
-	trace.areaType = areaType
-	trace.target = target
-	trace.propertyData = propertyData
-	trace.clickTime = datetime.now()
-	cookieId=request.COOKIES["cookieId"] if "cookieId" in request.COOKIES  else uuid.uuid4()
-	trace.cookieId = cookieId
-	trace.save()
-
-	response = HttpResponse('')
-	if "cookieId" not in request.COOKIES:
-		response.set_cookie("cookieId",cookieId)
-	return response
-
-def toWx(request,uuid):
-	toWxItem=LinkToWx.objects.filter(platForm__id=uuid).first()
-	contextDict={'wxText':toWxItem.wxText,'imgName':toWxItem.image}
-
-	templateName = "linkToWx.html"
-	staticFileName=toWxItem.platForm.name+".html"
-	staticView(contextDict, templateName,staticFileName)
-	return render(request, "static/" + staticFileName)
+    templateName = "index.html"
+    staticView(contextDict, templateName, templateName)
+    response = render(request, "static/"+templateName)
+    if "cookieId" not in request.COOKIES:
+        response.set_cookie("cookieId", uuid.uuid4())
+    return response
 
 
-def strongStaticView(contextDict, templateName,staticFileName):
-	static_html = 'templates/static/' + staticFileName
-	content = render_to_string('miniBond/' + templateName, contextDict)
-	with open(static_html, 'w', encoding="utf-8") as static_file:
-		static_file.write(content)
+def staticView(contextDict, templateName, staticFileName):
+    static_html = 'templates/static/' + staticFileName
+    if not os.path.exists(static_html) or True:
+        content = render_to_string('miniBond/' + templateName, contextDict)
+        with open(static_html, 'w', encoding="utf-8") as static_file:
+            static_file.write(content)
+
+
+def logTrace(request, areaType, target, propertyData):
+    trace = ClickTrace()
+    trace.areaType = areaType
+    trace.target = target
+    trace.propertyData = propertyData
+    trace.clickTime = datetime.now()
+    cookieId = request.COOKIES[
+        "cookieId"] if "cookieId" in request.COOKIES else uuid.uuid4()
+    trace.cookieId = cookieId
+    trace.save()
+
+    response = HttpResponse('')
+    if "cookieId" not in request.COOKIES:
+        response.set_cookie("cookieId", cookieId)
+    return response
+
+
+def toWx(request, uuid):
+    toWxItem = LinkToWx.objects.filter(platForm__id=uuid).first()
+    contextDict = {'wxText': toWxItem.wxText, 'imgName': toWxItem.image}
+
+    templateName = "linkToWx.html"
+    staticFileName = toWxItem.platForm.name+".html"
+    staticView(contextDict, templateName, staticFileName)
+    return render(request, "static/" + staticFileName)
+
+
+def strongStaticView(contextDict, templateName, staticFileName):
+    static_html = 'templates/static/' + staticFileName
+    content = render_to_string('miniBond/' + templateName, contextDict)
+    with open(static_html, 'w', encoding="utf-8") as static_file:
+        static_file.write(content)
+
 
 def refreshCache(request):
-	allpfs = Platform.objects.filter(isValid=True)
-	pfs = [p for p in allpfs if
-		   p.promotioninfo_set.filter(isValid=True).count() > 0 or (hasattr(p, 'linktowx') and p.linktowx.isValid)]
-	contextDict = {'pfs': pfs}
-	templateName = "index.html"
-	strongStaticView(contextDict, templateName, templateName)
+    allpfs = Platform.objects.filter(isValid=True)
+    pfs = [p for p in allpfs if
+           p.promotioninfo_set.filter(isValid=True).count() > 0 or (hasattr(p, 'linktowx') and p.linktowx.isValid)]
+    contextDict = {'pfs': pfs}
+    templateName = "index.html"
+    strongStaticView(contextDict, templateName, templateName)
 
-	allpfs = Platform.objects.all()
-	for pf in allpfs:
-		if pf.linktowx:
-			toWxItem = LinkToWx.objects.filter(platForm__id=pf.id).first()
-			contextDict = {'wxText': toWxItem.wxText, 'imgName': toWxItem.image}
+    allpfs = Platform.objects.all()
+    for pf in allpfs:
+        if hasattr(pf, 'linktowx'):
+            toWxItem = LinkToWx.objects.filter(platForm__id=pf.id).first()
+            contextDict = {'wxText': toWxItem.wxText,
+                           'imgName': toWxItem.image}
 
-			staticFileName = toWxItem.platForm.name + ".html"
-			strongStaticView(contextDict, "linkToWx.html", staticFileName)
+            staticFileName = toWxItem.platForm.name + ".html"
+            strongStaticView(contextDict, "linkToWx.html", staticFileName)
 
-	response = HttpResponse('well done!')
-	return response
+    response = HttpResponse('well done!')
+    return response
